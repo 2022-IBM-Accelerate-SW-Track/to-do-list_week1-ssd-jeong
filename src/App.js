0133904603fa8dbe73a808445b7ae3bc81678112
Daniel Jeong <sdjeong0404@gmail.com>
2022-06-08 20:15:16 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavbarComp from './component/navigation/NavbarComp';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <NavbarComp/>
+
     </div>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
-  
+import React, { Component } from "react";
+import "./About.css";
+import prof_pic from "../assets/IMG_4024.jpeg";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img
+              className="profile_image"
+              src={prof_pic}
+              alt="Profile Pic"
+            ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Daniel Jeong</div>
+            <div className="brief_description">
+              Hobbies: Squash, Stocks, and Violin
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
