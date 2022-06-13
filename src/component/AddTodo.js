0da8905916eef7f941c37d384be06c509d1ce21b
Daniel Jeong <sdjeong0404@gmail.com>
2022-06-12 22:13:34 -0400

    final

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -41,12 +41,12 @@ class AddTodo extends Component {
         <TextField
           label="Add New Item"
           variant="outlined"
-          onChange={ this.handleChange()}
+          onChange={this.handleChange()}
           value={this.setState()}
         />
         <Button
           style={{ marginLeft: "10px" }}
-          onClick={ this.handleSubmit()}
+          onClick={this.handleSubmit()}
           variant="contained"
           color="primary"
         >
